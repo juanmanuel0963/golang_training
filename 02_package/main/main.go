becf9dceb1dbd3e75dde5fc9bfb0aa795ad07ac3
juanmanuel0963 <juanmanuel0963@gmail.com>
2022-07-10 21:23:35 -0500

    commit

--- 02_package/main/main.go
+++ 02_package/main/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/GoesToEleven/GolangTraining/02_package/stringutil"
-	//someAlias "github.com/GoesToEleven/GolangTraining/02_package/icomefromalaska"
+	"stringutil"
+	//"github.com/GoesToEleven/GolangTraining/02_package/stringutil"
+	//"github.com/GoesToEleven/GolangTraining/02_package/stringutil"
+	//someAlias github.com/GoesToEleven/GolangTraining/02_package/icomefromalaska"
 )
 
 func main() {
 	fmt.Println(stringutil.Reverse("!oG ,olleH"))
 	fmt.Println(stringutil.MyName)
-	fmt.Println(winniepooh.BearName)
+	//fmt.Println(winniepooh.BearName)
 }
--- 02_package/stringutil/reverse.go
+++ 02_package/stringutil/reverse.go
@@ -6,6 +6,14 @@ func Reverse(s string) string {
 	return reverseTwo(s)
 }
 
+func reverseTwo(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
+}
+
 /*
 go build
 	go build reverse.go reverseTwo.go
